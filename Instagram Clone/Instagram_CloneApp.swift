e44c9a83b5a4d40900cecad14233f3469104ea3d
Hayronsan <hayronsan@gmail.com>
2024-04-25 00:38:53 +0500

    Initial Commit

---
+++ Instagram Clone/Instagram_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_CloneApp.swift
+//  Instagram Clone
+//
+//  Created by Nodir on 25/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
